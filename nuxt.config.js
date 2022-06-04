8de73783982ffbfe0f674bbc0bf9c8963885427a
boutchaz <zakaria.boutchamir@4d.com>
2022-06-05 00:02:11 +0100

    chore: update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Boutchamir zakaria , Dari Jihane et leurs familles,ont le plaisir de vous annoncer leur mariage' },
+      { hid: 'og:image', property: 'og:image', content:'/wedding.jpeg' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
