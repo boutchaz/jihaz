78b14fb79812973b2557c8c302cbf87d53b9cf94
boutchaz <zakaria.boutchamir@4d.com>
2022-06-05 00:34:11 +0100

    chore: add itemprop for whatsapp card

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Boutchamir zakaria , Dari Jihane et leurs familles,ont le plaisir de vous annoncer leur mariage' },
-      { hid: 'og:image', property: 'og:image', content:'/wedding.jpeg' }
+      { hid: 'og:image', property: 'og:image',itemprop:'image' ,content:'/wedding.jpeg' }
 
     ],
     link: [
